e5fdda0c4c350edb8ac284e9f7c636538512d1b3
zero0dayali <officialali35@gmail.com>
2020-09-27 21:36:30 +0700

    menampilkan data dari database

--- controller.js
+++ controller.js
@@ -4,5 +4,17 @@ var response = require('./res');
 var connection = require('./connection');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi nya jaan");
-}
+    response.ok("Aplikasi nya jalan",res);
+};
+
+//nampilin data nya dari database
+
+exports.tampilSemuaData = function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function (error, rows, fileds) {
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
+};
