b85c15331e97058571a2a161eb798aba013888a3
zero0dayali <officialali35@gmail.com>
2020-09-27 21:21:43 +0700

    settings routes in server

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ const port = 3000
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+var routes = require('./routes');
+routes(app);
+
 // app.get('/', (req, res) => res.send('Hello World!')) 
 app.listen(port, () => console.log(`Example app listening on port port!`))
