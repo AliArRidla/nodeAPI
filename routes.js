f220f30b353e23c73a7eaf3562fcabb438ae3d8c
zero0dayali <officialali35@gmail.com>
2020-09-28 07:15:40 +0700

    berhasil update dan delete data

--- routes.js
+++ routes.js
@@ -1,17 +1,25 @@
 'use strict';
 
-module.exports = function(app){
+module.exports = function (app) {
     var jsonku = require('./controller');
 
     app.route('/')
         .get(jsonku.index);
 
-    app.route('/tampilSemua')
-        .get(jsonku.tampilSemuaData);
+    app.route('/tampil')
+        .get(jsonku.tampilSemuaMahasiswa);
 
-    app.route('/tampilById/:id')
+    app.route('/tampil/:id')
         .get(jsonku.tampilById);
-
-    app.route('/tambahMahasiswa')
+    app.route('/tambah')
         .post(jsonku.tambahMahasiswa);
+
+    app.route('/ubah')
+        .put(jsonku.ubahMahasiswa);
+
+    app.route('/hapus')
+        .delete(jsonku.hapusMahasiswa);
+
+    app.route('/tampilmatakuliah')
+        .get(jsonku.tampilgroupmatakuliah);
 }
